f1be720a44d3d7aef9ee5139fe5b3d0059f12762
morrisxd <waifium@msn.com>
2015-10-19 21:21:14 +0800

    add patch.sh for building gcc-3.3

---
+++ morrix-project-name/cl.tar/patch.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install -y libc6_dev
+sudo apt-get install -y g++-multilib
+sudo apt-get install -y libc6-dev-i386
+sudo cp /usr/include/i386-linux-gnu/gnu/* /usr/include/gnu
+# http://stackoverflow.com/questions/7412548/error-gnu-stubs-32-h-no-such-file-or-directory-while-compiling-nachos-source
+# uname -m -p -i
+ls /usr/include/gnu
